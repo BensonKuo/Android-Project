4c4a99f4e81d486f52e4df8e21748c84a6d24636
bensonkuo <bensonkuo17@gmail.com>
2015-10-20 01:20:14 +0800

    resultactivity

--- BMI/app/src/main/java/net/lionfree/bensonkuo/bmi/ResultActivity.java
+++ BMI/app/src/main/java/net/lionfree/bensonkuo/bmi/ResultActivity.java
@@ -2,12 +2,23 @@ package net.lionfree.bensonkuo.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private TextView bmiText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        bmiText = (TextView)findViewById(R.id.bmiText);
+        Double w = Double.parseDouble(getIntent().getStringExtra("weight"));
+
+
     }
+
+
 }
