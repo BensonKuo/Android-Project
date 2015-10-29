feb64ab217befd50c149409b624732abd0330478
bensonkuo <bensonkuo17@gmail.com>
2015-10-29 08:54:29 +0800

    getLatLng() with 3 methods

--- BenUI/app/src/main/java/net/lionfree/bensonkuo/benui/OrderDetailActivity.java
+++ BenUI/app/src/main/java/net/lionfree/bensonkuo/benui/OrderDetailActivity.java
@@ -3,21 +3,46 @@ package net.lionfree.bensonkuo.benui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
 public class OrderDetailActivity extends AppCompatActivity {
 
+    private TextView addressTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
 
-        String store = getIntent().getStringExtra("storeInfo");
-        String address = store.split(",")[1];
-        Log.d("address", address);
+        addressTextView = (TextView)findViewById(R.id.address);
 
+        String store = getIntent().getStringExtra("storeInfo");
+        final String address = store.split(",")[1];
+        //Log.d("address", address);
+
+        addressTextView.setText(address);
+
+        // why new thread?
+        // this part will be replaced with AsyncTask
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String url = Utils.getGEOUrl(address);
+                final String result = new String(Utils.urlToBytes(url));
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // string ->jsonobj ->string
+                        addressTextView.setText(Utils.getLatLngFromJSON(result));
+
+                    }
+                });
+            }
+        }).start();
 
     }
 }
