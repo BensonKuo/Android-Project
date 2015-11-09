411d200213b1a5685d806c105912c3ee378c8673
bensonkuo <bensonkuo17@gmail.com>
2015-11-09 13:57:06 +0800

    add loginActivity & login button

---
+++ BenUI/app/src/main/java/net/lionfree/bensonkuo/benui/LoginActivity.java
@@ -0,0 +1,13 @@
+package net.lionfree.bensonkuo.benui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
