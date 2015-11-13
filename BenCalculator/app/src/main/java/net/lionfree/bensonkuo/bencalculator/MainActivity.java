9fdb2d6df1ecb8acb132bc5333dfe53e4a7f1003
bensonkuo <bensonkuo17@gmail.com>
2015-11-14 01:07:56 +0800

    inputNumber method

--- BenCalculator/app/src/main/java/net/lionfree/bensonkuo/bencalculator/MainActivity.java
+++ BenCalculator/app/src/main/java/net/lionfree/bensonkuo/bencalculator/MainActivity.java
@@ -3,8 +3,14 @@ package net.lionfree.bensonkuo.bencalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    private TextView CalcTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,8 +18,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    // when pressing number btn
     public void inputNumber(View view){
-        
+        switch (view.getId()) {
+            case R.id.one:
+                Toast.makeText(this,"one",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(this,"null",Toast.LENGTH_SHORT).show();
+        }
     }
 }
