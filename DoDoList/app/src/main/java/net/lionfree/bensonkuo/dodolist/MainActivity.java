3f023f4e7563eda2894acf82a8e75ea73e706646
bensonkuo <bensonkuo17@gmail.com>
2015-11-21 16:27:24 +0800

    delete all to remake by FragmentActivity

--- DoDoList/app/src/main/java/net/lionfree/bensonkuo/dodolist/MainActivity.java
+++ DoDoList/app/src/main/java/net/lionfree/bensonkuo/dodolist/MainActivity.java
@@ -1,13 +1,26 @@
 package net.lionfree.bensonkuo.dodolist;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
